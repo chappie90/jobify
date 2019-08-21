e3225359ee46e06d2d968259ac0b18a7246bf2cc
stoyangarov <stoyan.garov@yahoo.com>
2019-08-21 23:40:52 +0100

    Initial commit

---
+++ angular-client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  // Link to our api, pointing to localhost
+ // API = 'http://localhost:3000';
+
+}
---
+++ express-server/routes/api.js
@@ -0,0 +1,3 @@
+
+// MongoDB URL from the docker-compose file
+//const dbHost = 'mongodb://mongo/jobify';
