b1bd252a5d56397d1cb0c9795ebc91ab4d8ca984
stoyangarov <stoyan.garov@yahoo.com>
2019-08-22 22:36:17 +0100

    Add mongoose package and define mongo connection

--- express-server/app.js
+++ express-server/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://stoyangarov:Daspak12@emaily-w8ewa.mongodb.net/jobify-dev?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 module.exports = app;
