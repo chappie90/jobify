fba375a6acb5b39e098e763187f8ad897616c5c4
stoyangarov <stoyan.garov@yahoo.com>
2019-08-23 20:40:57 +0100

    Seperate header in three components

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavPrimaryComponent } from './header/nav-primary/nav-primary.component';
+import { NavSecondaryJobseekerComponent } from './header/nav-secondary-jobseeker/nav-secondary-jobseeker.component';
+import { NavSecondaryEmployerComponent } from './header/nav-secondary-employer/nav-secondary-employer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavPrimaryComponent,
+    NavSecondaryJobseekerComponent,
+    NavSecondaryEmployerComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-client/src/app/header/nav-secondary-jobseeker/nav-secondary-jobseeker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-secondary-jobseeker',
+  templateUrl: './nav-secondary-jobseeker.component.html',
+  styleUrls: ['./nav-secondary-jobseeker.component.scss']
+})
+export class NavSecondaryJobseekerComponent {
+
+}
