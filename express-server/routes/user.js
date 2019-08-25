1de92573298df4c2753c197d3d67bf35d3c34d74
stoyangarov <stoyan.garov@yahoo.com>
2019-08-26 00:29:03 +0100

    Add mongoose user model and install mongoose unique validator

--- express-server/routes/user.js
+++ express-server/routes/user.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  };
+ const user = new User({
+  email: req.body.email,
+  password: req.body.password
+ });
+ user.save()
+ .then(response => {
+    console.log(response);  
+ })
+ .catch(err => {
+    console.log(err);
+ });
   res.status(201).json({
     message: 'User created',
     response: user
