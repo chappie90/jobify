d566b05e7512514239da1dda67bd05fcb6d64f1f
stoyangarov <stoyan.garov@yahoo.com>
2019-08-26 00:40:21 +0100

    Modify user route to create mongo user document on signup

--- express-server/routes/user.js
+++ express-server/routes/user.js
@@ -1,25 +1,28 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
- const user = new User({
-  email: req.body.email,
-  password: req.body.password
- });
- user.save()
- .then(response => {
-    console.log(response);  
- })
- .catch(err => {
-    console.log(err);
- });
-  res.status(201).json({
-    message: 'User created',
-    response: user
-  });
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+      .then(response => {
+          res.status(201).json({
+            message: 'User created',
+            response: user
+          });  
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    });
 });
 
 module.exports = router;
