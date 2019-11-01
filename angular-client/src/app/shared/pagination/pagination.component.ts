b6af45d474b5765079fce389d2bb2205ece4face
stoyangarov <stoyan.garov@yahoo.com>
2019-11-01 13:08:19 +0000

    Add formatting to jobs list dates with template pipe

--- angular-client/src/app/shared/pagination/pagination.component.ts
+++ angular-client/src/app/shared/pagination/pagination.component.ts
@@ -36,7 +36,6 @@ export class PaginationComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.buildPagination();
     this.route.queryParams.subscribe(params => {
-      console.log('changed');
         let title = params.title;
         let location = params.location;
         let date = params.date;
@@ -134,39 +133,39 @@ export class PaginationComponent implements OnInit, OnDestroy {
   buildPagination() {
     let jobs = this.jobsService.returnAllJobsData();
     this.jobsCount = jobs.count;
-          this.selectedPage = parseInt(jobs.currentPage);
-          this.numberPages = Math.ceil(parseInt(this.jobsCount) / this.pageSize);
-          this.pages = [...Array(this.numberPages).keys()].map(x => ++x);
-          this.pagesOutput = [];
-          let lastPage = this.pages.slice(-1).pop();
-          if (this.pages.length < 10) {
-            this.pagesOutput = this.pages;
-          } else {
-             if (this.selectedPage <= 8) {
-            let pagesRange = this.pages.slice(0, 8);
-            for (let i = 0; i < pagesRange.length; i++) {
-              this.pagesOutput.push(pagesRange[i]);
-            }              
-            this.pagesOutput.push(this.moreJobs);
-            this.pagesOutput.push(lastPage);
-          } else if (this.selectedPage >= this.pages.slice(-1).pop() - 7) {
-            this.pagesOutput.push(1);
-            this.pagesOutput.push(this.moreJobs);
-            let pagesRange = this.pages.slice(this.pages.slice(-1).pop() - 8, this.pages.slice(-1).pop());
-            for (let i = 0; i < pagesRange.length; i++) {
-              this.pagesOutput.push(pagesRange[i]);
-            }
-          } else {
-            this.pagesOutput.push(1);
-            this.pagesOutput.push(this.moreJobs);
-            let pagesRange = this.pages.slice(this.firstPage, this.lastPage);
-            for (let i = 0; i < pagesRange.length; i++) {
-              this.pagesOutput.push(pagesRange[i]);
-            }
-            this.pagesOutput.push(this.moreJobs);
-            this.pagesOutput.push(this.pages.slice(-1).pop()); 
-          }
-          }
+    this.selectedPage = parseInt(jobs.currentPage);
+    this.numberPages = Math.ceil(parseInt(this.jobsCount) / this.pageSize);
+    this.pages = [...Array(this.numberPages).keys()].map(x => ++x);
+    this.pagesOutput = [];
+    let lastPage = this.pages.slice(-1).pop();
+    if (this.pages.length < 10) {
+      this.pagesOutput = this.pages;
+    } else {
+       if (this.selectedPage <= 8) {
+      let pagesRange = this.pages.slice(0, 8);
+      for (let i = 0; i < pagesRange.length; i++) {
+        this.pagesOutput.push(pagesRange[i]);
+      }              
+      this.pagesOutput.push(this.moreJobs);
+      this.pagesOutput.push(lastPage);
+    } else if (this.selectedPage >= this.pages.slice(-1).pop() - 7) {
+      this.pagesOutput.push(1);
+      this.pagesOutput.push(this.moreJobs);
+      let pagesRange = this.pages.slice(this.pages.slice(-1).pop() - 8, this.pages.slice(-1).pop());
+      for (let i = 0; i < pagesRange.length; i++) {
+        this.pagesOutput.push(pagesRange[i]);
+      }
+    } else {
+      this.pagesOutput.push(1);
+      this.pagesOutput.push(this.moreJobs);
+      let pagesRange = this.pages.slice(this.firstPage, this.lastPage);
+      for (let i = 0; i < pagesRange.length; i++) {
+        this.pagesOutput.push(pagesRange[i]);
+      }
+      this.pagesOutput.push(this.moreJobs);
+      this.pagesOutput.push(this.pages.slice(-1).pop()); 
+    }
+    }
   }
 
   onGetPage(page, index, previousPage, nextPage) {
